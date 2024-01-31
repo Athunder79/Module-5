c68b04b72be4031368091cc184910f3c38213f3b
Athunder79 <athunder@me.com>
2024-01-31 08:54:19 +0000

    Name chage

--- DiaTrack/DiaTrack/wsgi.py
+++ DiaTrack/DiaTrack/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Module5 project.
+WSGI config for DiaTrack project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Module5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DiaTrack.settings')
 
 application = get_wsgi_application()
